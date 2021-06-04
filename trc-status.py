02baf23f6af45b7fcf5f97171553d34272aa48f4
Ian Wojtowicz <ian@miscellaneousprojects.com>
2021-06-03 23:18:16 -0700

    Cleans up the code.

--- trc-status.py
+++ trc-status.py
@@ -7,18 +7,9 @@ import json
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# Urlencode the URL
-# url = urllib.parse.quote_plus("https://newsinteractives.cbc.ca/longform-single/beyond-94")
 url = "https://newsinteractives.cbc.ca/longform-single/beyond-94"
 
-# Create the query URL.
-query = "https://api.scraperbox.com/scrape"
-query += "?token=%s" % "4ACAF450EAC31DDD730B0FC04C1B18FC"
-query += "&url=%s" % url
-query += "&javascript_enabled=true"
-
 # Call the API.
-#request = urllib.request.Request(query)
 request = urllib.request.Request(url)
 raw_response = urllib.request.urlopen(request).read()
 html = raw_response.decode("utf-8")
@@ -27,10 +18,10 @@ html = raw_response.decode("utf-8")
 soup = BeautifulSoup(html, 'html.parser')
 
 # Find the elements
-totals = soup.select(".progress-row h3")
-totals = list(map(lambda x: int(x.getText().strip()), totals))
-complete = totals[-1]
-total = functools.reduce(lambda a, b: a+b, totals)
+counts = soup.select(".progress-row h3")
+counts = list(map(lambda x: int(x.getText().strip()), counts))
+total = functools.reduce(lambda a, b: a+b, counts)
+complete = counts[-1]
 
 # Get the text contents
 print(json.dumps({ "total" : total, "complete" : complete }))
